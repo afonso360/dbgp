1147f0e170441ab3dbe01e0dfe169b33a5fb35fa
Afonso Bordado <afonsobordado@az8.co>
2017-12-30 18:48:35 +0000

    Add License header

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 //#![deny(missing_docs)]
 //#![deny(warnings)]
 #![doc(test(attr(allow(unused_variables), deny(warnings))))]
